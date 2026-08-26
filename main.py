97d2353b1d45c3dfe2cc8a3d68a8ac3668d79a95
ochopi <53490489+ochopi@users.noreply.github.com>
2026-08-26 19:15:12 +0000

    testing testing

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "ochopi"
-    page.add(ft.Text("Hola Mundo!, Esto es ochopi.com 🎉"))
+    page.add(ft.Text("Hola Mundo!, Esto es ochopi.com 2.0 🎉"))
 
 ft.app(target=main)
